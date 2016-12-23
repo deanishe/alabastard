409ccc3f48f753c0a77fd4b69a786486a68983ea
Dean Jackson <deanishe@deanishe.net>
2016-12-23 19:34:27 +0100

    Adapt for homepage/blog, not docs

---
+++ static/js/alabastard.js
@@ -0,0 +1,10 @@
+// onload handler that supports IE versions < 9 as well as real browsers
+function callOnLoad(func) {
+  if (window.addEventListener) {
+    window.addEventListener('load', func, false);
+  } else if (window.attachEvent) {
+    window.attachEvent('onload', func);
+  } else {
+    window.onload = func;
+  }
+}
