32ed3a52d68455a0e0678d138fe63137ef2dae51
Dean Jackson <deanishe@deanishe.net>
2016-12-26 16:29:39 +0100

    Better icons and spacing
    - Use Octicons for Event icons
    - More space in lists
    - Move text away from icons in project list
    - Use moment.js throughout site
    - Lazy-load moment.js if needed

--- static/js/alabastard.js
+++ static/js/alabastard.js
@@ -9,3 +9,23 @@ function callOnLoad(func) {
     window.onload = func;
   }
 }
+
+function loadScript(url, callback) {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  if (script.readyState) {  // IE
+    script.onreadystatechange = function() {
+      if (script.readyState === 'loaded' || script.readyState === 'complete') {
+        script.onreadystatechange = null;
+        callback();
+      }
+    };
+  } else {  // other browsers
+    script.onload = function() {
+      callback();
+    };
+  }
+
+  script.src = url;
+  document.getElementsByTagName('body')[0].appendChild(script);
+}
